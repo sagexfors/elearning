5ccbc65f10c43b85abb38b32ec791ca5c71daceb
Adrian Rodriguez <zapxcero@gmail.com>
2022-11-15 23:16:17 +0800

    Add: Subject CR

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,9 +1,25 @@
 class SubjectsController < ApplicationController
   def index
     @subjects = Subject.all
+    @subject = Subject.new
   end
 
   def show
     @subject = Subject.find(params[:id])
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+
+    if @subject.save
+      redirect_to subjects_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :description)
   end
 end
