8babf539e0b4d09d9c1b24e0e54d64dde795785f
Adrian Rodriguez <zapxcero@gmail.com>
2022-11-20 14:56:33 +0800

    Validates Lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,5 @@
 class Lesson < ApplicationRecord
   belongs_to :subject
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
