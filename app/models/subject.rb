5b6686a9829bcf957c62d1b076008e7c0037ddfd
Adrian Rodriguez <zapxcero@gmail.com>
2022-11-20 19:04:51 +0800

    Add: Associated User and Subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,6 @@
 class Subject < ApplicationRecord
   has_many :lessons, dependent: :destroy
+  belongs_to :user
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { minimum: 10 }
 end
