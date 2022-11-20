f3571a90bd4b5010d09e91b157d4bca35ead8ddc
Adrian Rodriguez <zapxcero@gmail.com>
2022-11-20 15:06:21 +0800

    Shows No Lesson/Subjects if none

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,5 @@
 class Subject < ApplicationRecord
-  has_many :lessons
+  has_many :lessons, dependent: :destroy
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { minimum: 10 }
 end
