a461696e984c42463ce874d9272f2ef563ccc02d
Adrian Rodriguez <zapxcero@gmail.com>
2022-11-24 18:20:49 +0800

    Login Sign Up views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'lessons/new'
   get 'dashboards', to: 'dashboards#home'
-  root 'pages#home'
+  root 'dashboards#home'
 
   resources :subjects do
     resources :lessons
