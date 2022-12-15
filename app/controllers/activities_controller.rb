6aec91c236ea4643f68b7887ce01a09864ed5b32
Adrian Rodriguez <zapxcero@gmail.com>
2022-12-15 13:58:20 +0800

    Add activity edit

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -12,6 +12,18 @@ class ActivitiesController < ApplicationController
     redirect_to @subject, status: :see_other
   end
 
+  def edit
+    @subject = Subject.find(params[:subject_id])
+    @activity = @subject.activities.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:subject_id])
+    @activity = @subject.activities.find(params[:id])
+    @activity.update(activity_params)
+    redirect_to @subject, status: :see_other
+  end
+
   private
 
   def activity_params
