cfb84bbba05eb7b71e4f30a1b15268e124aec222
Adrian Rodriguez <zapxcero@gmail.com>
2023-01-08 16:24:57 +0800

    DRY out LessonsController and add Policy

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,35 +3,30 @@
 class LessonsController < ApplicationController
   layout 'dashboards'
 
+  before_action :authenticate_user!
+  before_action :set_subject, only: [:create, :destroy, :edit, :update]
+
   def index
-    if current_user.student?
-      @subjects = current_user.student_subjects
-    elsif current_user.teacher?
-      @subjects = current_user.teacher_subjects
-    end
+    return @subjects = current_user.student_subjects if current_user.student?
+
+    @subjects = current_user.teacher_subjects
+    @subject = Subject.new
   end
 
   def create
-    @subject = Subject.find(params[:subject_id])
-    @lesson = @subject.lessons.create(lesson_params)
+    @subject.lessons.create(lesson_params)
     redirect_to @subject
   end
 
   def destroy
-    @subject = Subject.find(params[:subject_id])
-    @lesson = @subject.lessons.find(params[:id])
     @lesson.destroy
     redirect_to @subject, status: :see_other
   end
 
   def edit
-    @subject = Subject.find(params[:subject_id])
-    @lesson = @subject.lessons.find(params[:id])
   end
 
   def update
-    @subject = Subject.find(params[:subject_id])
-    @lesson = @subject.lessons.find(params[:id])
     @lesson.update(lesson_params)
     redirect_to @subject, status: :see_other
   end
@@ -40,5 +35,10 @@ class LessonsController < ApplicationController
 
   def lesson_params
     params.require(:lesson).permit(:name, :description, :url)
+  end
+  
+  def set_subject
+    @subject = authorize Subject.find(params[:subject_id])
+    @lesson = @subject.lessons.find(params[:id])
   end
 end
