9a52a9e60bc87bb22668974cab67c23e5d11b5ca
Adrian Rodriguez <zapxcero@gmail.com>
2023-02-06 11:18:12 +0800

    Add students list

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,5 +2,6 @@ class StudentsController < ApplicationController
   before_action :authenticate_user!
   layout 'dashboards'
   def index
+    @students = current_user.teacher_students.uniq
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :enrollments, foreign_key: :student_id, class_name: "Enrollment"
   has_many :student_subjects, through: :enrollments, source: :subject
   has_many :teacher_subjects, foreign_key: :user_id, class_name: "Subject" #change to teacher_id
+  has_many :teacher_students, through: :teacher_subjects, source: :students
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: %i[student teacher admin]
