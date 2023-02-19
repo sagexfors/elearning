23c8abc9ec09feda71177c06f3ade8dc389897ed
Adrian Rodriguez <zapxcero@gmail.com>
2023-02-19 15:46:38 +0800

    Seed reduced

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 # Create a teacher and 10 students
 teacher = User.create(email: "teacher1@school.com", role: "teacher", password: "registration")
 
@@ -15,7 +6,7 @@ teacher = User.create(email: "teacher1@school.com", role: "teacher", password: "
 end
 
 # Create 5 subjects
-subjects = ["Math", "Science", "History", "English", "Art"]
+subjects = ["Math", "Science"]
 
 subjects.each do |subject|
   Subject.create(name: subject, description: "This is a description for #{subject}", user_id: teacher.id)
@@ -23,11 +14,9 @@ end
 
 subjects = Subject.all
 
-# create 2 lessons and 2 activites for each subject
+# create 1 lesson and 1 activity for each subject
 
 subjects.each do |subject|
   Lesson.create(name: "Lesson 1", description: "This is a description for Lesson 1", url: "https://drive.google.com", subject_id: subject.id)
-  Lesson.create(name: "Lesson 2", description: "This is a description for Lesson 2", url: "https://drive.google.com", subject_id: subject.id)
   Activity.create(name: "Activity 1", description: "This is a description for Activity 1", url: "https://drive.google.com", subject_id: subject.id)
-  Activity.create(name: "Activity 2", description: "This is a description for Activity 2", url: "https://drive.google.com", subject_id: subject.id)
 end
